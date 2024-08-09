ff0dba59e1eec39a4e1cfec5b02b14581bbb3615
Barath-Vikraman-D <135247354+Barath-Vikraman-D@users.noreply.github.com>
2024-08-09 22:55:04 +0530

    Create Maximize_Array_Value_After_Rearrangement.cpp

---
+++ 2024/August/09/Maximize_Array_Value_After_Rearrangement.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int Maximize(vector<int> &arr) {
+        unsigned long int res =0,MOD=1e9+7,index;
+        sort(arr.begin(),arr.end());
+        for( index=0;index<arr.size();index++)
+        {
+            res = ((res %MOD) + (arr[index]*index)) % MOD;
+        }
+        return res%MOD;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        //  cin.ignore();
+        int n = arr.size();
+        Solution ob;
+        cout << ob.Maximize(arr) << endl;
+    }
+}
+// } Driver Code Ends
